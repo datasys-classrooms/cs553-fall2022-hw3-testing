5c8e5ca510e2406e2e2736aa65810078f9db7495
Cerberus <aorhean@hawk.iit.edu>
2022-09-21 15:25:49 -0500

    update problem size

--- test-submission.sh
+++ test-submission.sh
@@ -24,22 +24,22 @@ get_checksum_from_data()
     echo $(head -n $2 test-data-$precision.txt | tail -n 1 | cut -d ' ' -f2)
 }
 
-TEST1="1. Test single precision 100000 flops with 1 thread                           "
-TEST1_ARGS="1 flops single 1000000 1"
-TEST2="2. Test double precision 100000 flops with 1 thread                           "
-TEST2_ARGS="1 flops double 1000000 1"
-TEST3="3. Test single precision 100000 flops with 8 thread                           "
-TEST3_ARGS="1 flops single 1000000 8"
-TEST4="4. Test double precision 100000 flops with 8 thread                           "
-TEST4_ARGS="1 flops double 1000000 8"
-TEST5="5. Test single precision 2048x2048 matrix multiplication with 1 thread          "
-TEST5_ARGS="1 matrix single 2048 1"
-TEST6="6. Test double precision 2048x2048 matrix multiplication with 1 thread          "
-TEST6_ARGS="1 matrix double 2048 1"
-TEST7="7. Test single precision 2048x2048 matrix multiplication with 8 threads         "
-TEST7_ARGS="1 matrix single 2048 8"
-TEST8="8. Test double precision 2048x2048 matrix multiplication with 8 threads         "
-TEST8_ARGS="1 matrix double 2048 8"
+TEST1="1. Test single precision 1000 flops with 1 thread                           "
+TEST1_ARGS="1 flops single 1000 1"
+TEST2="2. Test double precision 1000 flops with 1 thread                           "
+TEST2_ARGS="1 flops double 1000 1"
+TEST3="3. Test single precision 1000 flops with 8 thread                           "
+TEST3_ARGS="1 flops single 1000 8"
+TEST4="4. Test double precision 1000 flops with 8 thread                           "
+TEST4_ARGS="1 flops double 1000 8"
+TEST5="5. Test single precision 256x256 matrix multiplication with 1 thread          "
+TEST5_ARGS="1 matrix single 256 1"
+TEST6="6. Test double precision 256x256 matrix multiplication with 1 thread          "
+TEST6_ARGS="1 matrix double 256 1"
+TEST7="7. Test single precision 256x256 matrix multiplication with 8 threads         "
+TEST7_ARGS="1 matrix single 256 8"
+TEST8="8. Test double precision 256x256 matrix multiplication with 8 threads         "
+TEST8_ARGS="1 matrix double 256 8"
 
 NUM_TESTS=8
 
